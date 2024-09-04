2e89e06e280df2008622fe93f5e7ebceffa9d117
Melek Şevval ERDOĞAN <meleksevvalerdogan@gmail.com>
2024-09-04 04:10:42 +0300

    Database objects created

---
+++ bloggerMewl.Entities/Concrete/User.cs
@@ -0,0 +1,23 @@
+﻿using bloggerMewl.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bloggerMewl.Entities.Concrete
+{
+    public class User: EntityBase, IEntity
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+        public string Picture { get; set; }
+        public string Description { get; set; }
+        public ICollection<Article> Articles { get; set; }
+    }
+}
